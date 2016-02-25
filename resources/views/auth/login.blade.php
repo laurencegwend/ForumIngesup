3aaac193475548031b37ddf778b4593a035dc8a9
cdubant <charles.dubant@gmail.com>
2016-02-25 23:58:13 +0100

    Update on stylesheet.css and login.blade.php for styling

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,27 +11,21 @@
                 @endforeach
             </div>
         @endif
-
-        {!! Form::open(array('url' => 'auth/login','class'=>'form')) !!}
-        <div class="row row-form">
-            <div class="form-group">
-                {!! Form::label('email', 'Email') !!}
-                {!! Form::text('email', null, array('class' => 'form-control','placeholder' => 'firstname.lastname@ynov.com')) !!}
-            </div>
-            <div class="form-group">
-                {!! Form::label('password', 'Password') !!}
-                {!! Form::password('password', array('class' => 'form-control','placeholder' => '********')) !!}
-            </div>
-            {!! Form::submit('Sign In' , array('class' => 'btn btn-primary')) !!}
-        </div>
-        {!! Form::close() !!}
-
-        <div class="row">
+        <div class="row-form">
+            {!! Form::open(array('url' => 'auth/login','class'=>'form')) !!}
+                <div class="form-group">
+                    {!! Form::label('email', 'Email') !!}
+                    {!! Form::text('email', null, array('class' => 'form-control','placeholder' => 'firstname.lastname@ynov.com')) !!}
+                </div>
+                <div class="form-group">
+                    {!! Form::label('password', 'Password') !!}
+                    {!! Form::password('password', array('class' => 'form-control','placeholder' => '********')) !!}
+                </div>
+                {!! Form::submit('Sign In' , array('class' => 'btn btn-primary')) !!}
+            {!! Form::close() !!}
             <a href="#" class="forgot-password">Forgot Your Password</a>
         </div>
-    </div>
-    <div class="row row-register">
-        <div class="row">
+        <div class="row-register">
             <h5><strong>Don't Have an Account</strong></h5>
             <a href="/auth/register"><button class="btn btn-primary">Sign Up</button></a>
         </div>
