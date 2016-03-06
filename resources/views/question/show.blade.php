d2987c23399b3505ecc00ed7230a9c84af8f2bd3
Laurence Gwend <laurencegwend@yahoo.fr>
2016-03-06 18:43:02 +0100

    Amélioration de la vue question/show.blade.php

--- resources/views/question/show.blade.php
+++ resources/views/question/show.blade.php
@@ -9,16 +9,16 @@
                 @endforeach
             </div>
         @endif
-        <div class="row col-md-12 col-xs-12 question-list-header">
+        <div class="row col-md-12 col-xs-12 question-title">
                 <h2>{{$question->title}}</h2>
                 <div>
                     Created {{$question->created_at->diffForHumans()}} by <a href="#">{{$question->user->first_name}} {{$question->user->last_name}}</a>
                 </div>
         </div>
 
-        <div class="row col-md-12 col-xs-12 question-line question-content">{{$question->content}}</div>
+        <div class="row col-md-12 col-xs-12 question-content">{{$question->content}}</div>
 
-        <div class="row answer-list-header col-md-12 col-xs-12">{{$question->answers()->count()}} Answers</div>
+        <div class="row col-md-12 col-xs-12 answers-count">{{$question->answers()->count()}} Answer(s)</div>
 
         @foreach($answers as $answer)
             <div class="row answer-line col-md-12 col-xs-12">
@@ -28,15 +28,17 @@
 
         {!! Form::open(array('route' => array('answer.store', $question->id), 'method' => 'POST')) !!}
 
-        <div class="row your-answer">
+        <div class="row col-md-12 col-xs-12">
+            <div class="your-answer">
             {!! Form::label('content', 'Your Answer', array('class' => 'label-answer')) !!}
             {!! Form::textarea('content', null, array('class' => 'form-control')) !!}
-        </div>
-        <div class="row pull-right">
-        {!! Form::submit('Send', array('class' => 'btn btn-primary label-answer')) !!}
+            </div>
+            <div class="row pull-right">
+            {!! Form::submit('Send', array('class' => 'btn btn-primary label-answer')) !!}
+            </div>
         </div>
 </div>
-<script>
+<script type="text/javascript">
     CKEDITOR.replace( 'content' );
 </script>
 
